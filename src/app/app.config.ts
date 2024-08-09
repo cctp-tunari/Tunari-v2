613fd0c416d92f5186d12d8058cd58f756b52acf
Andres Bustamante <andresxbd@gmail.com>
2024-08-09 18:21:31 -0400

    add admin auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"tunari-65fdc","appId":"1:484537151866:web:e0d065daa179bf9c3526c3","storageBucket":"tunari-65fdc.appspot.com","apiKey":"AIzaSyC0GpkiZ4KcT6GvGZ4arJh-cCt2Agarbkc","authDomain":"tunari-65fdc.firebaseapp.com","messagingSenderId":"484537151866","measurementId":"G-GLPT50FQ7G"})), provideAuth(() => getAuth())]
 };
--- src/app/website/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/website/iniciar-sesion/iniciar-sesion.component.ts
@@ -2,23 +2,67 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import {FormsModule} from "@angular/forms";
 import { AppComponent } from '../../app.component';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-iniciar-sesion',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './iniciar-sesion.component.html',
   styleUrl: './iniciar-sesion.component.scss'
 })
+
 export class IniciarSesionComponent {
-  constructor(private router: Router, private appComponent: AppComponent) {}
+
+  public email: any = "";
+  public password: any = "";
+  public showAlert = false;
+  public message: any;
+
+
+
+  constructor(private router: Router, private appComponent: AppComponent, private userService: UserService) {}
 
   onLogin() {
+    console.log('click ', this.email, this.password)
+    this.userService.login(this.email,this.password)
+      .then(response => {
+        console.log("success");
+        this.router.navigate(['/admin']);
+
+      })
+      .catch(error => {
+        console.log(error);
+        this.openErrorAlert()
+
+      })
+
     // Aquí puedes agregar la lógica para validar el formulario y autenticar al usuario
-    this.appComponent.setLoginStatus(true); // Actualiza el estado de autenticación
-    // Redirigir a la página de administración
-    this.router.navigate(['/admin']); // Cambia '/admin' por la ruta a la que quieres navegar
+    // this.appComponent.setLoginStatus(true); // Actualiza el estado de autenticación
+    // // Redirigir a la página de administración
+    // this.router.navigate(['/admin']); // Cambia '/admin' por la ruta a la que quieres navegar
+  }
+
+
+  add(){
+    // console.log('click')
+    let email= "a@gmail.com";
+    let password= "123456789";
+    this.userService.register(email,password);
+  }
+
+  closeAlert() {
+    this.showAlert = false;
+  }
+
+  openErrorAlert() {
+    this.showAlert = true;
+    setTimeout(() => {
+      this.closeAlert();
+    }, 4000); 
   }
 }
