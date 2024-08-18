95c215b6678aedcff4087d3d6295c6927ec2064a
Leonardo Morales <leonardo.morales@keepcalling.com>
2024-08-18 15:11:16 -0400

    updates on 'cursos' page

--- src/app/website/cursos/cursos.component.ts
+++ src/app/website/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-cursos',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.scss'
 })
-export class CursosComponent {
+export class CursosComponent  implements OnInit {
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  navigateTo(course: string) {
+    this.router.navigate([course]);
+  }
 
 }
